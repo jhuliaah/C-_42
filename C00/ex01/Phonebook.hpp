440854f1308cc01d111fff4bc6d8b125fab53c14
Jhulia Alves De Carvalho <jhualves@c1r9p1.42sp.org.br>
2025-11-02 19:49:19 -0300

    vai fazer as funções vagabunda

--- C00/ex01/Phonebook.hpp
+++ C00/ex01/Phonebook.hpp
@@ -13,9 +13,9 @@ class	Phonebook{
 	private:
 
 	int						number_of_contacts;
-	Contacts_class			Contacts[8];
+	Contact_Class			Contacts[8];
 
-	Contact::Contact_class	get_contact(int id);
+	Contact_Class			get_contact(int id);
 	std::string 			truncate_string(std::string& str) const;
 	std::string				get_input(std::string& prompt) const;
 	std::string				display_contact_truncate(int id);
