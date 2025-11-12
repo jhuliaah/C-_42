64b004ab54b1c181a238982b2e3f107dd77c2084
Jhulia Carvalho <115580017+jhuliaah@users.noreply.github.com>
2025-11-12 17:59:42 -0300

    hihi

--- C01/ex01/ZombieHorde.cpp
+++ C01/ex01/ZombieHorde.cpp
@@ -10,7 +10,8 @@ Zombie	*zombieHorde(int N, std::string name)
 	for (int i = 0; i < N; i++)
 	{
 		horde[i].setName(name);
-		std:cout << "A new body arises from the dead, " << _name << "is reborn a Zombie" << std:: endl;
+		std::cout << "A new body arises from the dead, " << horde[i]._name << "is reborn a Zombie" << std:: endl;
 	}
 	return horde;
 }
+
