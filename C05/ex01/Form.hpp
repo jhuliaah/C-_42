d32844284cfc582ddf45ba469b166e9a6032e7fe
jhulia <jhuliaah@hotmail.com>
2026-08-30 18:34:22 -0300

    CPP05 ex01 - creating form

---
+++ C05/ex01/Form.hpp
@@ -0,0 +1,25 @@
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Form : Bureaucrat {
+
+    private:
+    std::string const _name;
+    bool _signed;
+
+    
+
+
+};
+
+
+
+
+
+#endif
